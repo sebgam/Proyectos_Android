f50f7e58f3adc30662b7e5568748197414810dbe
sebas <sebastiangamboa11@gmail.com>
2017-03-20 14:42:47 -0500

    main

--- Interfax_java/app/src/main/java/com/sebas/desarrollog/interfaz_java/MainActivity.java
+++ Interfax_java/app/src/main/java/com/sebas/desarrollog/interfaz_java/MainActivity.java
@@ -1,58 +1,37 @@
 package com.sebas.desarrollog.interfaz_java;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+
+    public int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-           }
-        });
+        contador = 0;
 
-       /* TextView mitexto= new TextView(this);s
-        mitexto.setText("hola putos");//--------------asigna texto
-        setContentView(mitexto);//----------vista que mostrara la actividad
-        */
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void incrementarContador(View vista){
+
+        contador++;
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void restaContador(View vista){
+        contador--;
     }
+
+    public void reseteaContador(View vista){
+        contador =0;
+    }
+
+
+
 }
