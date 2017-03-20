085b6414c9d6d1ac9f3cde89ddc290970b4f4d89
sebas <sebastiangamboa11@gmail.com>
2017-03-20 15:42:12 -0500

    main

--- Interfax_java/app/src/main/java/com/sebas/desarrollog/interfaz_java/MainActivity.java
+++ Interfax_java/app/src/main/java/com/sebas/desarrollog/interfaz_java/MainActivity.java
@@ -3,6 +3,7 @@ package com.sebas.desarrollog.interfaz_java;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -21,15 +22,23 @@ public class MainActivity extends Activity {
     public void incrementarContador(View vista){
 
         contador++;
+        mostrarResultado();
 
     }
 
     public void restaContador(View vista){
         contador--;
+        mostrarResultado();
     }
 
     public void reseteaContador(View vista){
         contador =0;
+        mostrarResultado();
+    }
+
+    public void mostrarResultado(){
+       TextView textoResultado = (TextView)findViewById(R.id.txtCon);
+        textoResultado.setText("Contador: " + contador);
     }
 
 
